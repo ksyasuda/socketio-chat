bb8dacdd26fd0ac6c71e476a21fcd5daec3abb15
ksyasuda <ksyasuda@umich.edu>
2021-03-09 19:06:20 -0500

    finish tutorial

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,13 @@ app.get("/", (req: express.Request, res: express.Response) => {
 
 io.on("connection", (socket: Socket) => {
 	console.log("a user connected");
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
+
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", msg);
+	});
 });
 
 server.listen(PORT, () => {
